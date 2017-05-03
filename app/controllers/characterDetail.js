1c940efd3ec4d30524b04806bec8488cf3d5c680
Adam Paxton <adampaxton@polancomedia.com>
2017-05-03 11:41:55 -0400

    set movie list in characterDetail

--- app/controllers/characterDetail.js
+++ app/controllers/characterDetail.js
@@ -23,22 +23,40 @@ $.detail1.text = 'Birth Year: ' + character.get('birth_year');
  * do some example of making a promise chain to fetch these details
  */
 
-xhr.send({
-    url: character.get('species')[0]
-})
-.then(function(res){
-    $.detail2.text = 'Species: ' + res.name + ' ('+ res.classification + ')';
-})
-.then(function(){
-    return xhr.send({
-       url: character.get('homeworld')
+//not all characters have a species for some reason, so do a check
+//to see if the array is empty or not
+var speciesArray = character.get('species');
+var speciesUrl = (speciesArray && speciesArray.length > 0) ? speciesArray[0] : '';
+
+if(speciesUrl){
+    xhr.send({
+        url: speciesUrl
+    })
+    .then(function (res) {
+        $.detail2.text = 'Species: ' + res.name + ' (' + res.classification + ')';
     });
+} else {
+    $.detail2.text = 'Species: unknown';
+}
+
+xhr.send({
+    url: character.get('homeworld')
 })
 .then(function(res){
     $.detail3.text = 'Homeworld: ' + res.name;
 });
 
 
+function dataFilter(collection){
+    //since we already have the films loaded in our collection
+    //lets avoid a round trip to fetch them just for this character
+    //and filter them instead
+    var films = character.get('films');
+
+    return collection.filter(function(film){
+        return films.indexOf(film.get('url')) === -1 ? false : true;
+    });
+}
 
 
 function closeWindow() {
@@ -48,3 +66,9 @@ function closeWindow() {
 if (OS_ANDROID) {
     $.win.addEventListener('android:back', closeWindow);
 }
+
+Alloy.Collections.films.trigger('change');
+
+$.win.addEventListener('close', function () {
+    $.destroy();
+});
